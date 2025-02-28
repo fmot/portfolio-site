ea3c2ba49db6e11d3a4c23f94e6f90d6740f530a
fmot <genzzabro09812@gmail.com>
2025-02-28 10:30:32 -0800

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,77 @@ import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  return <></>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="header-content">
+          {menuOpen ? (
+            <>
+              <h1 className="site-title">Fumiya Otake</h1>
+              <button className="close-btn" onClick={toggleMenu}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="100"
+                  height="100"
+                  viewBox="0 0 50 50"
+                >
+                  <path d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 32.990234 15.986328 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.990234 15.986328 z"></path>
+                </svg>
+              </button>
+            </>
+          ) : (
+            <>
+              <h1 className="site-title">Fumiya Otake</h1>
+              <button className="menu-btn" onClick={toggleMenu}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="100"
+                  height="100"
+                  viewBox="0 0 50 50"
+                >
+                  <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
+                </svg>
+              </button>
+            </>
+          )}
+        </div>
+
+        {menuOpen && (
+          <nav className="menu">
+            <ul>
+              <li>
+                <a href="#home">ABOUT</a>
+              </li>
+              <li>
+                <a href="#experience">EXPERIENCE</a>
+              </li>
+              <li>
+                <a href="#skills">SKILLS</a>
+              </li>
+              <li>
+                <a href="#bprojects">PROJECTS</a>
+              </li>
+              <li>
+                <a href="#contact">CONTACT</a>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
+
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
