2b5e8209d29642599af3005a633ae6d2baa14a1e
fmot <genzzabro09812@gmail.com>
2025-03-02 14:09:42 -0800

    Modify experience section

--- src/App.jsx
+++ src/App.jsx
@@ -284,6 +284,23 @@ function App() {
               </div>
             </div>
           </div>
+
+          <button class="resume-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="200"
+              height="200"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M11.78 7.159a.75.75 0 0 0-1.06 0l-1.97 1.97V1.75a.75.75 0 0 0-1.5 0v7.379l-1.97-1.97a.75.75 0 0 0-1.06 1.06l3.25 3.25L8 12l.53-.53l3.25-3.25a.75.75 0 0 0 0-1.061ZM2.5 9.75a.75.75 0 0 0-1.5 0V13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9.75a.75.75 0 0 0-1.5 0V13a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V9.75Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span>Download resume</span>
+          </button>
         </section>
 
         <section className="tech-stack">
@@ -387,7 +404,7 @@ function App() {
                       </g>
                     </svg>
                   </span>
-                  <span>Javascript</span>
+                  <span>JavaScript</span>
                 </div>
                 <div className="skill-item">
                   <span className="skill-icon">
